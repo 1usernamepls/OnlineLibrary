11a91da5418180b1a26f680539148cad4f3e837a
Ethan E Lopez <etlopez@chapman.edu>
2024-12-04 12:03:33 -0800

    Create BookShop.java

---
+++ BookShop.java
@@ -0,0 +1,28 @@
+public class BookShop {
+
+    public static void main(String[] args) {
+
+        String email = "gmop@chapman.edu";
+        String username = "THE14u";
+        String password = "hello123!";
+        String firstName = "Paul";
+        String lastName = "Sanderson";
+
+        User a = new User(email, username, password, firstName, lastName);
+        System.out.println(a);
+
+        // String title = "Harry Potter And The Sourcerer's Stone";
+        // String author = "J.K. Rowling";
+        // String language = "English";
+        // int publishing = 1997;
+        // double price = 6.99;
+        // boolean availability = true;
+        // String genre = "FANTASY";
+        // boolean bestseller = true;   
+        
+        // Fiction b = new Fiction(title, author, language, publishing, price, availability, genre, bestseller);
+        // System.out.println(b);
+
+    }
+    
+}
