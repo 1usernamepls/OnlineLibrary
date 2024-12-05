5f46cbf34cff3ed9346fba4a5920a7d5808d4cb4
Aidan Tran <146998556+AidanTranc@users.noreply.github.com>
2024-12-04 23:38:59 -0800

    Update Nonfiction.java

--- Nonfiction.java
+++ Nonfiction.java
@@ -1,22 +1,38 @@
 public class Nonfiction extends Book {
-    
-    protected String genre; // "History," "Science," "Biography," etc.
 
-    public Nonfiction(String t, String auth, String l, int pub, double p, boolean a, int s, String c, String genre) {
-        super(t, auth, l, pub, p, a, s, c); 
-        this.genre = genre; 
+    protected String genre; // "History," "Science," "Biography," etc.
+    protected int edition; // Edition of the book
+
+    public Nonfiction(String t, String auth, String l, int pub, double p, boolean a, int s, String genre, int edition) {
+        super(t, auth, l, pub, p, a, s, "Nonfiction");
+        this.genre = genre;
+        this.edition = edition;
     }
+
 
     public String getGenre() {
         return genre;
     }
-  
+
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
-    public String toString() {
-        return super.toString() + "      Genre: " + genre + "\n";
+   
+    public int getEdition() {
+        return edition;
     }
 
+    public void setEdition(int edition) {
+        if (edition > 0) { // Ensure the edition is valid
+            this.edition = edition;
+        } else {
+            throw new IllegalArgumentException("Edition must be a positive number.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "      Genre: " + genre + "\n      Edition: " + edition + "\n";
+    }
 }
