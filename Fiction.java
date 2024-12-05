4dbdef69941f4ad07a97538afc4d8d7703b0fb98
Aidan Tran <146998556+AidanTranc@users.noreply.github.com>
2024-12-04 23:36:45 -0800

    Update Fiction.java

--- Fiction.java
+++ Fiction.java
@@ -1,12 +1,15 @@
 public class Fiction extends Book {
 
-    protected boolean bestseller;
+    protected boolean bestseller; 
+    protected String genre; 
 
-    public Fiction(String t, String auth, String l, int pub, double p, boolean a, int s, String c, boolean b) {
-        super(t, auth, l, pub, p, a, s, c);
-        this.bestseller = b;
+    public Fiction(String t, String auth, String l, int pub, double p, boolean a, int s, String c, String genre, boolean bestseller) {
+        super(t, auth, l, pub, p, a, s, c); 
+        this.genre = genre; 
+        this.bestseller = bestseller; 
     }
 
+ 
     public boolean isBestseller() {
         return bestseller;
     }
@@ -15,7 +18,18 @@ public class Fiction extends Book {
         this.bestseller = bestseller;
     }
 
+   
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+  
+    @Override
     public String toString() {
-        return super.toString() + "      Bestseller: " + (bestseller ? "Yes" : "No") + "\n";
+        return super.toString() + "      Genre: " + genre + "\n      Bestseller: " + (bestseller ? "Yes" : "No") + "\n";
     }
 }
