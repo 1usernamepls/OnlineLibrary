993fef01b7947554760fd1c20ae7b9991f6eb2d6
Ethan E Lopez <etlopez@chapman.edu>
2024-12-04 16:03:52 -0800

    Update Nonfiction.java

--- Nonfiction.java
+++ Nonfiction.java
@@ -1,26 +1,20 @@
 public class Nonfiction extends Book {
-
     
-    private String genre; // "History," "Science," "Biography," etc.
+    protected String genre; // "History," "Science," "Biography," etc.
 
-
-    public Nonfiction(String t, String auth, String l, int pub, double p, boolean a, int s, String genre) {
-        super(t, auth, l, pub, p, a, s, "Nonfiction"); 
+    public Nonfiction(String t, String auth, String l, int pub, double p, boolean a, int s, String c, String genre) {
+        super(t, auth, l, pub, p, a, s, c); 
         this.genre = genre; 
     }
-
 
     public String getGenre() {
         return genre;
     }
-
-    
+  
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
-  
-    @Override
     public String toString() {
         return super.toString() + "      Genre: " + genre + "\n";
     }
