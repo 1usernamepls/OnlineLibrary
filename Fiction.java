ffeb2730669ce324c19298df80819e379b76f1bf
Aidan Tran <146998556+AidanTranc@users.noreply.github.com>
2024-12-08 12:58:31 -0800

    Update Fiction.java

--- Fiction.java
+++ Fiction.java
@@ -1,15 +1,16 @@
 public class Fiction extends Book {
-
-    protected String genre; 
     protected boolean bestseller; 
+    protected String genre; //horror, sci-fi, etc
+    protected String ageGroup; // target audience 
 
-    public Fiction(String t, String auth, String l, int pub, double p, boolean a, int s, String genre, boolean bestseller) {
-        super(t, auth, l, pub, p, a, s, "FICTION"); 
+    public Fiction(String t, String auth, String l, int pub, double p, boolean a, int s, String genre, boolean bestseller, String ageGroup) {
+        super(t, auth, l, pub, p, a, s, "Fiction"); 
         this.genre = genre; 
         this.bestseller = bestseller; 
+        this.ageGroup = ageGroup;
     }
 
- 
+    //bestSeller setter/getters
     public boolean isBestseller() {
         return bestseller;
     }
@@ -18,7 +19,7 @@ public class Fiction extends Book {
         this.bestseller = bestseller;
     }
 
-   
+    //genre setter/getters
     public String getGenre() {
         return genre;
     }
@@ -27,8 +28,20 @@ public class Fiction extends Book {
         this.genre = genre;
     }
 
+    //ageGroup setters/getters
+    public String getAgeGroup() {
+        return ageGroup;
+    }
+
+    public void setAgeGroup(String ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+
     @Override
     public String toString() {
-        return super.toString() + "      Genre: " + genre + "\n      Bestseller: " + (bestseller ? "Yes" : "No") + "\n";
+        return super.toString() 
+                + "      Genre: " + genre + "\n" 
+                + "      Bestseller: " + (bestseller ? "Yes" : "No") + "\n"
+                + "      Age Group: " + ageGroup + "\n";
     }
 }
