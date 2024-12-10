6f6d903795d70004f046cb993f766edbea32bb99
Ethan E Lopez <etlopez@chapman.edu>
2024-12-09 19:22:01 -0800

    Update Nonfiction.java

--- Nonfiction.java
+++ Nonfiction.java
@@ -2,12 +2,14 @@ public class Nonfiction extends Book {
     protected String genre; //bibliography, literature review, historical, etc
     protected int edition;
     protected boolean isPeerReviewed; // true/false
+    protected String topic;
 
-    public Nonfiction(String t, String auth, String l, int pub, double p, boolean a, int s, String genre, int edition, boolean isPeerReviewed) {
+    public Nonfiction(String t, String auth, String l, int pub, double p, boolean a, int s, String genre, int edition, boolean isPeerReviewed, String topic) {
         super(t, auth, l, pub, p, a, s, "Nonfiction");
         this.genre = genre;
         this.edition = edition;
         this.isPeerReviewed = isPeerReviewed;
+        this.topic = topic;
     }
 
     public String getGenre() {
@@ -37,15 +39,23 @@ public class Nonfiction extends Book {
     public void setPeerReviewed(boolean isPeerReviewed) {
         this.isPeerReviewed = isPeerReviewed;
     }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String t) {
+        this.topic = t;
+    }
     
     //Nonfiction specific target audience 
     @Override
     public String getTargetAudience() {
-        return "Target Audience: Readers interested in " + genre + " (Peer Reviewed: " + (isPeerReviewed ? "Yes" : "No") + ").";
+        return "Target Audience: Readers interested in " + topic + " (Peer Reviewed: " + (isPeerReviewed ? "Yes" : "No") + ").";
     }
 
     @Override
     public String toString() {
-        return super.toString() + "\nGenre: " + genre + "\nEdition: " + edition + "\n" + getTargetAudience();
+        return super.toString() + "\nGenre: " + genre + "\nEdition: " + edition + "\n" + getTargetAudience() + "\n";
     }
 }
