8f1c07bd340d2259e078a6f214f77859cda072b4
Ethan E Lopez <etlopez@chapman.edu>
2024-12-10 13:16:26 -0800

    Update User.java

--- User.java
+++ User.java
@@ -18,12 +18,24 @@ public abstract class User {
         return email;
     }
 
+    public void setEmail(String e) {
+        email = e;
+    }
+
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String u) {
+        username = u;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String p) {
+        password = p;
     }
 
     public String getName() {
